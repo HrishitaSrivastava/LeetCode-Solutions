0cfbfb20df6ce48bf85ef53a0f8758b5e6dd0a48
Hrishita Srivastava <hrishitasrivastava1106@gmail.com>
2022-10-23 22:24:19 +0530

    Update 0001.java
    Added a second solution for the problem

--- Java/0001.java
+++ Java/0001.java
@@ -13,3 +13,23 @@ class Solution {
     return new int[] {-1,-1};
     }
 }
+// Solution 2
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        int result[] = new int[2];
+        for (int i = 0; i<len;i++)
+        {
+            for (int j = i+1; j<len; j++)
+            {
+                if (nums[i]+nums[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+            
+        }
+        return (result);
+    }
+}
